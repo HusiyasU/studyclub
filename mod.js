a82e035953abe73dbb079b7cb181faf4f722ef69
Iyastama <111851974+HusiyasU@users.noreply.github.com>
2025-04-10 20:07:41 +0800

    C1

---
+++ mod.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+  }
+
+function changeCharacter(src) {
+    document.getElementById("mainCharacter").src = src;
+  }
+  
